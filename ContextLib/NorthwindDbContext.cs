f960071e2b792049e25e97abfcb0b93702b41188
unknown <omitogunjesufemi@gmail.com>
2022-07-08 09:42:47 +0100

    Setting up of the Northwind Project

---
+++ ContextLib/NorthwindDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+
+namespace NorthwindLibrary 
+{
+    public class NorthwindDbContext : DbContext 
+    {
+        public NorthwindDbContext (DbContextOptions<NorthwindDbContext> options) : base (options) 
+        { 
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
